eac29dd68d097d2e0f5e251ca003c18e42febf2f
DahaKot <kotova.ds@phystech.edu>
2023-11-12 23:29:08 +0400

    Fixed dataloader, wrote perplexity calculation in batches

--- calc_metrics.py
+++ calc_metrics.py
@@ -1,4 +1,3 @@
-import pathlib
 # from datasets import load_dataset
 import torch
 from transformers import AutoModelForCausalLM, LlamaTokenizer, BitsAndBytesConfig, AutoTokenizer
@@ -8,33 +7,24 @@ from transformers import AutoModelForCausalLM, LlamaTokenizer, BitsAndBytesConfi
 # from peft import LoraConfig, prepare_model_for_kbit_training
 from peft import PeftModel 
 from args_parser import get_args
-import re
 from pathlib import Path
 from utils import *
 from tqdm import tqdm
 
-import numpy as np
-import evaluate
+tmp=0.9
+top_p=0.6
+max_length=1024
 
 if __name__ == "__main__":
     args = get_args()
 
-    # for arg in vars(args):
-    #     print(arg, getattr(args, arg))
-
-
-
-    # Get the datasets
-    train_dataset, val_dataset, test_dataset = get_datasets(args.train_dataset_source_path, args.train_dataset_target_path,
-                               args.val_dataset_source_path, args.val_dataset_target_path,
-                               args.test_dataset_source_path, args.test_dataset_target_path,
-                               field = args.field)
-
-    val_dataset = val_dataset[:2]
+    dataset = get_dataset(
+        args.source_path, args.target_path, field="prompt", prompt_only=True
+    )
 
     model_name = args.model_name
 
-    print("1. Loaded datasets.")
+    print("1. Loaded dataset.")
 
     ## Bits and Bytes config
     bnb_config = BitsAndBytesConfig(
@@ -55,64 +45,76 @@ if __name__ == "__main__":
     # m = m.merge_and_unload()
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
     tokenizer.bos_token_id = 1
+    tokenizer.pad_token = tokenizer.eos_token
     stop_token_ids = [0]
 
     print(f"2. Successfully loaded the model {model_name} into memory")
 
 
     output_dir = args.output_dir
-
-
     output_dir = args.output_dir + f"/{model_name.split('/')[-1]}"
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     print(f"Saving the model to {output_dir}")
 
-    # for name, module in trainer.model.named_modules():
-    #     if "norm" in name:
-    #         module = module.to(torch.float32)
-
-
-    # if args.checkpoint_path:
-    #     trainer.train(args.checkpoint_path)
-    # else:
-    #     trainer.train()
-
-    # model.load_state_dict(args.checkpoint_path)
-    # print(val_dataset.keys())
-    encodings = tokenizer("\n\n".join(val_dataset["prompt"][0]), return_tensors="pt")
-
-    max_length = 4096
-    stride = 512
-    seq_len = encodings.input_ids.size(1)
-    device = "cuda"
-
-    print("before perplexity estimation.")
+    dataset.set_format(type="torch")
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.per_device_val_batch_size
+    )
 
     nlls = []
-    prev_end_loc = 0
-    for begin_loc in tqdm(range(0, seq_len, stride)):
-        end_loc = min(begin_loc + max_length, seq_len)
-        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-        input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-        target_ids = input_ids.clone()
-        target_ids[:, :-trg_len] = -100
+    for batch in tqdm(dataloader):
+        inputs = tokenizer(batch["prompt"], padding=True, return_tensors="pt").to('cuda')
 
+        with torch.inference_mode():
+            outputs = model.generate(
+                **inputs, max_new_tokens=max_length, do_sample=True,
+                top_p=top_p, temperature=tmp
+            )
+        # tok.decode(outputs[0][inputs_length:], skip_special_tokens=True)
         with torch.no_grad():
-            outputs = model(input_ids)
-            
-            # loss is calculated using CrossEntropyLoss which averages over valid labels
-            # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
-            # to the left by 1.
-            neg_log_likelihood = outputs.loss
+            model_output = model(outputs, labels=outputs)
+
+            neg_log_likelihood = model_output.loss
 
         nlls.append(neg_log_likelihood)
 
-        if begin_loc == 0:
-            print("in the loop")
-
-        prev_end_loc = end_loc
-        if end_loc == seq_len:
-            break
-
     ppl = torch.exp(torch.stack(nlls).mean())
     print("Final perplexity is: ", ppl)
+
+    # encodings = tokenizer("\n\n".join(dataset["prompt"]), return_tensors="pt")
+
+    # max_length = 4096
+    # stride = 512
+    # seq_len = encodings.input_ids.size(1)
+    # device = "cuda"
+
+    # print("before perplexity estimation.")
+
+    # nlls = []
+    # prev_end_loc = 0
+    # for begin_loc in tqdm(range(0, seq_len, stride)):
+    #     end_loc = min(begin_loc + max_length, seq_len)
+    #     trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+    #     input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+    #     target_ids = input_ids.clone()
+    #     target_ids[:, :-trg_len] = -100
+
+    #     with torch.no_grad():
+    #         outputs = model(input_ids)
+            
+    #         # loss is calculated using CrossEntropyLoss which averages over valid labels
+    #         # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+    #         # to the left by 1.
+    #         neg_log_likelihood = outputs.loss
+
+    #     nlls.append(neg_log_likelihood)
+
+    #     if begin_loc == 0:
+    #         print("in the loop")
+
+    #     prev_end_loc = end_loc
+    #     if end_loc == seq_len:
+    #         break
+
+    # ppl = torch.exp(torch.stack(nlls).mean())
+    # print("Final perplexity is: ", ppl)
--- utils.py
+++ utils.py
@@ -64,17 +64,21 @@ def generate_text(data_point, field, train):
         field: generate_training_prompt(idea, story, train),
     }
 
-def get_dataset(source_path, target_path, field = 'prompt'):
-
-    dataset_source = load_dataset('text',data_files=source_path, split="train")
-    dataset_target = load_dataset('text',data_files=target_path, split="train")
+def get_dataset(source_path, target_path, field='prompt', prompt_only=False):
+    dataset_source = load_dataset('text', data_files=source_path, split="train")
+    dataset_target = load_dataset('text', data_files=target_path, split="train")
 
     dataset_source =  dataset_source.rename_column("text", "source_text")
     dataset_target = dataset_target.rename_column("text", "target_text")
 
-    dataset = dataset_source.add_column("target_text", dataset_target['target_text']) 
+    dataset = dataset_source.add_column(
+        "target_text", dataset_target['target_text']
+    ) 
 
-    dataset = dataset.map(generate_text, fn_kwargs={"field": field}, remove_columns=["source_text", "target_text"])
+    dataset = dataset.map(
+        generate_text, fn_kwargs={"field": field,"train": False},
+        remove_columns=["source_text", "target_text"]
+    )
 
     return dataset
      
