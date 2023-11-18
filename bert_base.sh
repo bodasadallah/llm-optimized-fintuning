28eabffa78461902843155ea4ef38f401ae073af
profii <anastasiademidov@mail.ru>
2023-11-18 12:41:05 +0400

    baseline bertscore

--- bert_base.sh
+++ bert_base.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#SBATCH --job-name=bert # Job name
+#SBATCH --job-name=bert_base # Job name
 #SBATCH --error=/home/anastasiia.demidova/llm/llm-optimized-fintuning/logs/%j%x.err # error file
 #SBATCH --output=/home/anastasiia.demidova/llm/llm-optimized-fintuning/logs/%j%x.out # output log file
 #SBATCH --time=24:00:00 # 10 hours of wall time
 #SBATCH --nodes=1  # 1 GPU node
 #SBATCH --mem=46000 # 32 GB of RAM
-#SBATCH --nodelist=ws-l5-008
+#SBATCH --nodelist=ws-l1-008
 
 
 echo "starting......................."
 
 
-MODEL_NAME="Llama-2-7b-hf"
+# MODEL_NAME="Llama-2-7b-hf"
 # MODEL_NAME="jais"
-# MODEL_NAME="Mistral-7B-v0.1"
+MODEL_NAME="Mistral-7B-v0.1"
 
 
 echo $MODEL_NAME
-python calc_metrics_bert_copy.py
+python calc_metrics_bert_copy.py --model_name="mistralai/Mistral-7B-v0.1"
 
-# # --checkpoint_path="experiments/Mistral-7B-v0.1/checkpoint-24000" 
+#--model_name="meta-llama/Llama-2-7b-hf" 
 
 
 echo " ending "
--- calc_metrics_bert_copy.py
+++ calc_metrics_bert_copy.py
@@ -1,13 +1,10 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
-from peft import PeftModel#, prepare_model_for_int8_training #get_peft_config, prepare_model_for_training
 from args_parser import get_args
 from pathlib import Path
 from utils import *
 from tqdm import tqdm
 from evaluate import load
-# from huggingface_hub import hf_hub_download
-# import itertools
 
 tmp=0.9
 top_p=0.6
@@ -35,20 +32,6 @@ if __name__ == "__main__":
         bnb_4bit_use_double_quant=True
     )
 
-    # lora_alpha = args.lora_alpha
-    # lora_dropout = args.lora_dropout
-    # lora_r = args.lora_r
-    # lora_target_modules = args.lora_target_modules
-
-    # peft_config = LoraConfig(
-    #     lora_alpha=lora_alpha,
-    #     lora_dropout=lora_dropout,
-    #     r=lora_r,
-    #     bias="none",
-    #     task_type="CAUSAL_LM",
-    #     target_modules = lora_target_modules
-    # )
-
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         quantization_config=bnb_config,
@@ -56,33 +39,17 @@ if __name__ == "__main__":
         device_map='auto'
     )
 
-    # model = prepare_model_for_int8_training(model)
-    # model = prepare_model_for_training(model)
-
-    # peft_config = get_peft_config(config)
-
     # model = AutoModelForCausalLM.from_pretrained(
     #     model_name,
     #     load_in_4bit=True,
     #     torch_dtype=torch.bfloat16,
     #     device_map='auto'
     # )
-    # model = PeftModel.from_pretrained(model, args.checkpoint_path)
-    
-    # config_file = hf_hub_download(model_name, 'adapter_config.json')
 
-    model_tuple = PeftModel.from_pretrained(model, model_id='/home/anastasiia.demidova/.cache/huggingface/hub/models--meta-llama--Llama-2-7b-hf'), #config=peft_config) # Baseline
-    # model = PeftModel.from_pretrained(model, model_id='/home/anastasiia.demidova/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.1'), # Baseline
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.bos_token_id = 1
     tokenizer.pad_token = tokenizer.eos_token
     stop_token_ids = [0]
-
-    # print(model)
-    
-    # model = model_tuple[0]
-    # model = tf.convert_to_tensor(model)
-    model.eval()
 
     print(f"2. Successfully loaded the model {model_name} into memory")
 
@@ -111,13 +78,11 @@ if __name__ == "__main__":
         # if i >= 161:
 
         inputs = tokenizer(batch["prompt"], padding=True, return_tensors="pt").to('cuda')
-        # inputs = tokenizer(batch["prompt"], padding=True, return_tensors="pt").input_ids
 
         # with torch.no_grad():
         with torch.inference_mode():
             outputs = model.generate(
                 **inputs, max_new_tokens=max_length, do_sample=True,
-                # input_ids=inputs.to("cuda"), max_new_tokens=max_length, do_sample=True,
                 top_p=top_p, temperature=tmp,
             )
 
@@ -140,7 +105,7 @@ if __name__ == "__main__":
             print(f1)
             
         if i % 10 == 0:
-            with open(args.output_dir + f"/{model_name.split('/')[-1]}" + '/bertscore.txt', 'a') as f:
+            with open(args.output_dir + f"/{model_name.split('/')[-1]}" + '/bertscore_base.txt', 'a') as f:
                 f.write(str(i+1) + ' ' + str(pres) + ' ' + str(reca) + ' ' + str(f1) + '\n')
                 f.flush()
 
@@ -149,7 +114,7 @@ if __name__ == "__main__":
         # else:
         #     i += 1
 
-    with open(args.output_dir + f"/{model_name.split('/')[-1]}" + '/bertscore.txt', 'a') as f:
+    with open(args.output_dir + f"/{model_name.split('/')[-1]}" + '/bertscore_base.txt', 'a') as f:
                 f.write(str(i) + ' ' + str(pres) + ' ' + str(reca) + ' ' + str(f1) + '\n')
                 f.flush()
                 
